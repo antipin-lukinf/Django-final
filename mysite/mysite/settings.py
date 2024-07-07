3106ff7706cdd93180d49f94c5a9857efaff82e1
Drei4 <antipin-lukinf@mail.ru>
2024-07-07 18:16:00 +0300

    complit1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ruolh@h1x_efl=()$8m-n)*7ku)lb^j$r5$y&_53#-ld9@okhq'
+# SECRET_KEY = 'django-insecure-ruolh@h1x_efl=()$8m-n)*7ku)lb^j$r5$y&_53#-ld9@okhq'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['antipinlukinf.pythonanywhere.com',]
 
 
 # Application definition
@@ -81,10 +84,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '< antipinlukinf>$<antipinlukinf$default>',
+        'USER': '< antipinlukinf>',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': '<antipinlukinf.mysql.pythonanywhere-services.com>',
+        'OPTIONS': {
+                    'init_command': "SET NAMES 'utf8mb4';SET sql_mode) = 'STRICT_TRANS_TABLES'", 'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -124,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static'),
 ]
